5fe6c0ba557bc6bd2415117b10e9bb482b2b8e87
Bernardo Vieira <bernardo5@live.com.pt>
2020-04-06 10:00:13 +0100

    load basic info to community tab

--- tab/Community.tsx
+++ tab/Community.tsx
@@ -4,22 +4,67 @@ import {
     Text,
     View,
 } from 'react-native';
+import ImpactMarketContractABI from '../contracts/ImpactMarketABI.json'
+import ContractAddresses from '../contracts/network.json';
+import { connect, ConnectedProps } from 'react-redux';
+import { IRootState } from '../helpers/types';
 
 
-interface ICommunityState {
+interface ICommunityProps {
 }
-export default class Community extends React.Component<{}, ICommunityState> {
+const mapStateToProps = (state: IRootState) => {
+    const { users } = state
+    return { users }
+};
+
+const connector = connect(mapStateToProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux & ICommunityProps
+interface ICommunityState {
+    loading: boolean;
+    userInCommunity: string;
+}
+class Community extends React.Component<Props, ICommunityState> {
 
     constructor(props: any) {
         super(props);
         this.state = {
+            loading: false,
+            userInCommunity: '',
         }
     }
 
+    componentDidMount = () => {
+        const { address } = this.props.users.celoInfo;
+        const demoCommunityAddress = '0x27c719785c899A7426Af69Fee2d01091e38b7588';
+        const instance = new this.props.users.kit.web3.eth.Contract(
+            ImpactMarketContractABI as any,
+            ContractAddresses.alfajores.ImpactMarket,
+        );
+        instance.methods.userToCommunity(address, demoCommunityAddress).call()
+            .then((result: boolean) => {
+                if (result) {
+                    this.setState({ userInCommunity: demoCommunityAddress });
+                }
+            })
+    }
+
     render() {
+        const { loading, userInCommunity } = this.state;
+        let message = '';
+        if (loading) {
+            return <Text>Loading...</Text>
+        }
+        if (userInCommunity.length > 0) {
+            message = 'You belong to community ' + userInCommunity
+        } else {
+            message = 'You do not belong to a community!'
+        }
         return (
             <View style={styles.container}>
-                <Text>You do not belong to a community!</Text>
+                <Text>{message}</Text>
             </View>
         );
     }
@@ -33,3 +78,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     }
 });
+
+export default connector(Community);
