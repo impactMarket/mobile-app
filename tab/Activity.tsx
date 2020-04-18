dd44f28c50057c478b801e57e921bb09b40f6947
Bernardo Vieira <bernardo5@live.com.pt>
2020-04-18 15:20:50 +0100

    dummy activity dinamically

--- tab/Activity.tsx
+++ tab/Activity.tsx
@@ -7,6 +7,7 @@ import {
 import { connect, ConnectedProps } from 'react-redux';
 import { IRootState } from '../helpers/types';
 import { Appbar, Avatar, List } from 'react-native-paper';
+import { ethers } from 'ethers';
 
 
 interface IActivityProps {
@@ -21,19 +22,49 @@ const connector = connect(mapStateToProps)
 type PropsFromRedux = ConnectedProps<typeof connector>
 
 type Props = PropsFromRedux & IActivityProps
+interface IActivityListItem {
+    from: string;
+    description: string;
+    ammount: string;
+    status: string;
+    timestamp: number;
+}
 interface IActivityState {
+    activities: IActivityListItem[];
 }
 class Activity extends React.Component<Props, IActivityState> {
 
     constructor(props: any) {
         super(props);
         this.state = {
+            activities: [],
         }
     }
 
     // TODO: load activity history
+    componentDidMount = () => {
+        const activities = [
+            {
+                from: 'Fehsolna',
+                description: 'Brasil',
+                ammount: '2',
+                status: 'Claimed',
+                timestamp: 17263188,
+            },
+            {
+                from: 'Fehsolna',
+                description: 'Brasil',
+                ammount: '3',
+                status: 'Claimed',
+                timestamp: 17263179,
+            }
+        ];
+        activities.sort((a, b) => a.timestamp - b.timestamp);
+        this.setState({ activities });
+    }
 
     render() {
+        const { activities } = this.state;
         return (
             <View>
                 <Appbar.Header style={styles.appbar}>
@@ -45,24 +76,16 @@ class Activity extends React.Component<Props, IActivityState> {
                     <Appbar.Action icon="bell" />
                 </Appbar.Header>
                 <View>
-                    <List.Item
-                        title="Fehsolna"
-                        description="Brasil"
-                        left={() => <Avatar.Text size={46} label="F" />}
+                    {activities.map((activity) => <List.Item
+                        key={activity.timestamp}
+                        title={activity.from}
+                        description={activity.description}
+                        left={() => <Avatar.Text size={46} label={activity.from.slice(0, 1)} />}
                         right={() => <View>
-                            <Text>€2</Text>
-                            <Text>Claimed</Text>
+                            <Text>€{activity.ammount}</Text>
+                            <Text>{activity.status}</Text>
                         </View>}
-                    />
-                    <List.Item
-                        title="Fehsolna"
-                        description="Brasil"
-                        left={() => <Avatar.Text size={46} label="F" />}
-                        right={() => <View>
-                            <Text>€2</Text>
-                            <Text>Claimed</Text>
-                        </View>}
-                    />
+                    />)}
                 </View>
             </View>
         );
