82f887522e34e11a5ca1356f371aa487bd08fe6e
Bernardo Vieira <bernardo5@live.com.pt>
2020-04-24 20:03:54 +0100

    update login component to use redux provider

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,26 +5,53 @@ import {
     Text,
     Button,
     View,
+    AsyncStorage,
 } from 'react-native';
 import {
     requestAccountAddress,
     waitForAccountAuth,
 } from '@celo/dappkit'
 import { Linking } from 'expo'
-import { ILoginCallbackAnswer } from '../helpers/types';
+import {
+    ILoginCallbackAnswer,
+    STORAGE_USER_ADDRESS,
+    STORAGE_USER_PHONE_NUMBER,
+    SET_USER_WALLET_BALANCE,
+    STORAGE_USER_FIRST_TIME,
+    IRootState,
+} from '../helpers/types';
+import { setUserCeloInfo, setUserFirstTime } from '../helpers/ReduxActions';
+import { ConnectedProps, connect } from 'react-redux';
 
 
 interface ILoginProps {
-    loginCallback: (loginCallbackAnswer?: ILoginCallbackAnswer) => void;
 }
+const mapStateToProps = (state: IRootState) => {
+    const { user, network } = state
+    return { user, network }
+};
+
+const connector = connect(mapStateToProps)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux & ILoginProps
 interface ILoginState {
 }
-export default class Login extends React.Component<ILoginProps, ILoginState> {
+class Login extends React.Component<Props, ILoginState> {
 
     constructor(props: any) {
         super(props);
         this.state = {
         }
+    }
+
+    getCurrentUserBalance = async (address: string) => {
+        const stableToken = await this.props.network.kit.contracts.getStableToken()
+
+        const [cUSDBalanceBig, cUSDDecimals] = await Promise.all([stableToken.balanceOf(address), stableToken.decimals()])
+        let cUSDBalance = cUSDBalanceBig.div(10 ** 18).toFixed(2)
+        return cUSDBalance;
     }
 
     login = async () => {
@@ -40,20 +67,26 @@ export default class Login extends React.Component<ILoginProps, ILoginState> {
 
         const dappkitResponse = await waitForAccountAuth(requestId)
         try {
-            this.props.loginCallback({
-                celoInfo: {
-                    address: dappkitResponse.address,
-                    phoneNumber: dappkitResponse.phoneNumber,
-                    balance: '0',
-                }
-            })
+            const cUSDBalance = await this.getCurrentUserBalance(dappkitResponse.address);
+            //
+            await AsyncStorage.setItem(STORAGE_USER_ADDRESS, dappkitResponse.address);
+            await AsyncStorage.setItem(STORAGE_USER_PHONE_NUMBER, dappkitResponse.phoneNumber);
+            await AsyncStorage.setItem(STORAGE_USER_FIRST_TIME, 'false');
+            this.props.dispatch(setUserCeloInfo({
+                address: dappkitResponse.address,
+                phoneNumber: dappkitResponse.phoneNumber,
+                balance: cUSDBalance,
+            }))
+            this.props.dispatch(setUserFirstTime(false));
         } catch (error) {
             // Error saving data
+            console.log(error);
         }
     }
-
-    openWithoutLogin = () => {
-        this.props.loginCallback();
+    
+    openWithoutLogin = async () => {
+        await AsyncStorage.setItem(STORAGE_USER_FIRST_TIME, 'false');
+        this.props.dispatch(setUserFirstTime(false));
     }
 
     render() {
@@ -85,3 +118,5 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     }
 });
+
+export default connector(Login);
