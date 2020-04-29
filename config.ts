cf009f30f4ba1c6fdc9c5593196a75331b65ebcb
Bernardo Vieira <bernardo5@live.com.pt>
2020-04-29 19:41:00 +0100

    add base config

---
+++ config.ts
@@ -0,0 +1,11 @@
+export default {
+    /**
+     * The default API URL
+     */
+    baseApiUrl: 'http://192.168.0.209:5000/api',
+
+    /**
+     * JSON RPC url
+     */
+    jsonRpc: 'https://alfajores-forno.celo-testnet.org'
+};
