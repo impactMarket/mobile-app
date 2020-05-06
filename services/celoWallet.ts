bdfe07c42563b0e63a459e34d37debcdce228aa7
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-06 19:50:07 +0100

    refactor services

---
+++ services/celoWallet.ts
@@ -0,0 +1,37 @@
+import { requestTxSig, FeeCurrency, waitForSignedTxs } from "@celo/dappkit";
+import { INetworkState } from "../helpers/types";
+import { Linking } from "expo";
+import { toTxResult } from "@celo/contractkit/lib/utils/tx-result";
+
+
+async function celoWalletRequest(
+    from: string,
+    to: string,
+    txObject: any,
+    requestId: string,
+    network: INetworkState,
+): Promise<any> {
+    const dappName = 'Impact Market'
+    const callback = Linking.makeUrl('/my/path')
+
+    requestTxSig(
+        network.kit,
+        [
+            {
+                from,
+                to,
+                tx: txObject,
+                feeCurrency: FeeCurrency.cUSD
+            }
+        ],
+        { requestId, dappName, callback }
+    )
+
+    const dappkitResponse = await waitForSignedTxs(requestId);
+    const tx = dappkitResponse.rawTxs[0];
+    toTxResult(network.kit.web3.eth.sendSignedTransaction(tx)).waitReceipt();
+}
+
+export {
+    celoWalletRequest,
+}
--- services/index.ts
+++ services/index.ts
@@ -1,146 +1,14 @@
-import axios from 'axios';
-import config from '../config';
-import { ICommunity, IBeneficiaryRequest } from '../helpers/types';
-
-
-axios.defaults.baseURL = config.baseApiUrl;
-
-async function getAllValidCommunities(): Promise<ICommunity[]> {
-    let response = [] as ICommunity[];
-    try {
-        // handle success
-        const result = await axios.get('/community/all/valid')
-        response = result.data;
-    } catch (error) {
-        // handle error
-    } finally {
-        // always executed
-    }
-    return response;
-}
-
-async function requestCreateCommunity(
-    requestByAddress: string,
-    name: string,
-    description: string,
-    location: {
-        title: string,
-        latitude: number,
-        longitude: number,
-    },
-    coverImage: string,
-    txCreationObj: any,
-): Promise<boolean> {
-    let response = 500;
-    try {
-        // handle success
-        const requestBody = {
-            requestByAddress,
-            name,
-            description,
-            location,
-            coverImage,
-            txCreationObj,
-        };
-        const requestHeaders = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
-        };
-        const result = await axios.post('/community/request', requestBody, requestHeaders);
-        response = result.status;
-    } catch (error) {
-        // handle error
-    } finally {
-        // always executed
-    }
-    return response === 200 ? true : false;
-}
-
-async function requestJoinAsBeneficiary(
-    walletAddress: string,
-    communityPublicId: string,
-): Promise<boolean> {
-    let response = 500;
-    try {
-        // handle success
-        const requestBody = {
-            walletAddress,
-            communityPublicId
-        };
-        const requestHeaders = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
-        };
-        const result = await axios.post('/beneficiary/request', requestBody, requestHeaders);
-        response = result.status;
-    } catch (error) {
-        // handle error
-    } finally {
-        // always executed
-    }
-    return response === 200 ? true : false;
-}
-
-async function getBeneficiariesRequestByCommunity(
-    communityPublicId: string,
-): Promise<IBeneficiaryRequest[]> {
-    let response = [] as IBeneficiaryRequest[];
-    try {
-        const result = await axios.get(`/beneficiary/${communityPublicId}`);
-        response = result.data;
-    } catch (error) {
-        // handle error
-    } finally {
-        // always executed
-    }
-    return response;
-}
-
-async function acceptBeneficiaryRequest(
-    acceptanceTransaction: string,
-    communityPublicId: string,
-): Promise<boolean> {
-    let response = 500;
-    try {
-        // handle success
-        const requestBody = {
-            acceptanceTransaction,
-            communityPublicId
-        };
-        const requestHeaders = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
-        };
-        const result = await axios.post('/beneficiary/accept', requestBody, requestHeaders);
-        response = result.status;
-    } catch (error) {
-        // handle error
-    } finally {
-        // always executed
-    }
-    return response === 200 ? true : false;
-}
-
-async function getCommunityByContractAddress(
-    communityContractAddress: string,
-): Promise<ICommunity | undefined> {
-    let response: ICommunity = undefined as any;
-    try {
-        const result = await axios.get(`/community/address/${communityContractAddress}`);
-        response = result.data as ICommunity;
-    } catch (error) {
-        // handle error
-    } finally {
-        // always executed
-    }
-    return response;
-}
+import {
+    getAllValidCommunities,
+    requestCreateCommunity,
+    requestJoinAsBeneficiary,
+    getBeneficiariesRequestByCommunity,
+    acceptBeneficiaryRequest,
+    getCommunityByContractAddress,
+} from './api';
+import {
+    celoWalletRequest,
+} from './celoWallet'
 
 export {
     getAllValidCommunities,
@@ -149,4 +17,6 @@ export {
     getBeneficiariesRequestByCommunity,
     acceptBeneficiaryRequest,
     getCommunityByContractAddress,
+    //
+    celoWalletRequest,
 }
