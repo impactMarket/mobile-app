904a2e5edaae623fb3d733effc8a06ae3929edb2
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-12 19:49:34 +0100

    fix metro

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,16 @@
+const crypto = require.resolve('crypto-browserify')
+const url = require.resolve('url/')
 module.exports = {
 	resolver: {
 		extraNodeModules: {
-			crypto: require.resolve('crypto-browserify'),
-			url: require.resolve('url/'),
+			crypto,
+			url,
 			fs: require.resolve('expo-file-system'),
 			http: require.resolve('stream-http'),
 			https: require.resolve('https-browserify'),
 			os: require.resolve('os-browserify/browser.js'),
 			stream: require.resolve('readable-stream'),
-			vm: require.resolve('vm-browserify')
-		}
-	}
-};
+			vm: require.resolve('vm-browserify'),
+		},
+	},
+}
