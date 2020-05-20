ce488784f3b19c3ad03837c4d8a7f6e0cf116d99
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-20 17:14:43 +0100

    fix calculateCommunityProgress

--- helpers/index.ts
+++ helpers/index.ts
@@ -6,10 +6,14 @@ export function calculateCommunityProgress(
     toCalculte: string /*'raised' | 'claimed'*/,
     community: ICommunityInfo
 ): number {
+    // cUSD has 18 zeros!
+    const decimals = new ethers.utils.BigNumber(10).pow(18);
     if (toCalculte === 'raised') {
         const m = new ethers.utils.BigNumber(community.vars._claimHardCap).mul(community.beneficiaries.length);
-        return new ethers.utils.BigNumber(community.totalRaised).div(m.eq(0) ? 1 : m).toNumber();
+        const result = new ethers.utils.BigNumber(community.totalRaised).div(m.eq(0) ? 1 : m);
+        return result.div(decimals).toNumber();
     }
-    return new ethers.utils.BigNumber(community.totalClaimed)
-        .div(new ethers.utils.BigNumber(community.totalRaised).eq(0) ? 1 : community.totalRaised).toNumber();
+    const result = new ethers.utils.BigNumber(community.totalClaimed)
+        .div(new ethers.utils.BigNumber(community.totalRaised).eq(0) ? 1 : community.totalRaised);
+    return result.div(decimals).toNumber();
 }
