dca7866e33d2be35a2a4f31e3506b652588449ce
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-20 22:42:28 +0100

    AppEntry.js

---
+++ AppEntry.js
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import 'expo/build/Expo.fx';
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { activateKeepAwake } from 'expo-keep-awake';
+
+import App from './App';
+
+if (__DEV__) {
+  activateKeepAwake();
+}
+
+registerRootComponent(App);
