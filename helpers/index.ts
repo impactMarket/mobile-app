572f1b143ffa543103a4df23c725a7a40a96558c
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-20 14:48:31 +0100

    fix division by 0

--- helpers/index.ts
+++ helpers/index.ts
@@ -6,8 +6,8 @@ export function calculateCommunityProgress(
     community: ICommunityViewInfo
 ): number {
     if (toCalculte === 'raised') {
-        return community.totalRaised.div(community.vars!._claimHardCap
-            .mul(community.beneficiaries.length)).toNumber();
+        const m = community.vars!._claimHardCap.mul(community.beneficiaries.length);
+        return community.totalRaised.div(m.eq(0) ? 1 : m).toNumber();
     }
     return community.totalClaimed
         .div(community.totalRaised.eq(0) ? 1 : community.totalRaised).toNumber();
