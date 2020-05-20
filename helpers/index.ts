e6059938dbfacca4ca8531170d39530c9d753db5
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-20 15:55:12 +0100

    fix calculateCommunityProgress

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,14 +1,15 @@
-import { ICommunityViewInfo } from "./types";
+import { ICommunityInfo } from "./types";
+import { ethers } from "ethers";
 
 
 export function calculateCommunityProgress(
     toCalculte: string /*'raised' | 'claimed'*/,
-    community: ICommunityViewInfo
+    community: ICommunityInfo
 ): number {
     if (toCalculte === 'raised') {
-        const m = community.vars!._claimHardCap.mul(community.beneficiaries.length);
-        return community.totalRaised.div(m.eq(0) ? 1 : m).toNumber();
+        const m = new ethers.utils.BigNumber(community.vars._claimHardCap).mul(community.beneficiaries.length);
+        return new ethers.utils.BigNumber(community.totalRaised).div(m.eq(0) ? 1 : m).toNumber();
     }
-    return community.totalClaimed
-        .div(community.totalRaised.eq(0) ? 1 : community.totalRaised).toNumber();
+    return new ethers.utils.BigNumber(community.totalClaimed)
+        .div(new ethers.utils.BigNumber(community.totalRaised).eq(0) ? 1 : community.totalRaised).toNumber();
 }
