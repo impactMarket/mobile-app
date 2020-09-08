21493b29639ceb6ffca7ebb00398f7bf1ce8e02a
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-08 20:41:17 +0100

    faq pt

--- src/views/common/FAQScreen.tsx
+++ src/views/common/FAQScreen.tsx
@@ -1,10 +1,84 @@
 import { useNavigation } from '@react-navigation/native';
 import Header from 'components/Header';
-import * as Linking from 'expo-linking';
-import { iptcColors } from 'helpers/index';
 import React, { useState } from 'react';
-import { StyleSheet, ScrollView, View, Image } from 'react-native';
+import { StyleSheet, ScrollView, View } from 'react-native';
 import { List, Text } from 'react-native-paper';
+
+const ptFAQ = [
+    {
+        title: 'O que é a impactMarket?',
+        message:
+            'impactMarket é uma plataforma desenhada para facilitar o acesso e distribuição de fundos junto de quem mais precisa, através das suas comunidades.',
+    },
+    {
+        title:
+            'O que é o Rendimento Básico Incondicional (ou Universal Basic Income)?',
+        message:
+            'Esta forma de rendimento pressupõe que um conjunto de pessoas, seja do mesmo país, cidade ou mesmo comunidade local, tem acesso, de forma incondicional, a um rendimento recorrente suficiente para sair da pobreza.',
+    },
+    {
+        title: 'Por que é que a impactMarket usa a tecnologia Blockchain?',
+        message:
+            'A tecnologia blockchain permite um conjunto de novos benefícios que são especialmente interessantes para quem mais precisa. Esta tecnologia, permite que qualquer pessoa envie dinheiro para outra pessoa, de forma não censurável, transparente, sem intermediários, instantaneamente e quase gratuitamente, independentemente do país. Apenas é necessário uma ligação à internet e um smartphone para poder ter uma conta, enviar e receber dinheiro.',
+    },
+    {
+        title: 'O que posso fazer na ImpactMarket?',
+        message:
+            'Pode criar uma comunidade UBI e adicionar os seus beneficiários que terão acesso aos fundos que forem depositados nessa comunidade, de forma igual para todos. Para apoiar as comunidades que mais gosta, pode enviar $cUSD diretamente para o seu contrato.',
+    },
+    {
+        title: 'O que é necessário para poder começar a utilizar?',
+        message:
+            'Deverá instalar a app Valora e fazer o registo, caso queira criar uma comunidade ou aceder a um rendimento básico. A Valora permite o envio e receção de dinheiro, assim como a interação com a impactMarket. Caso deseje apenas contribuir, a instalação da Valora não é obrigatória.',
+    },
+    {
+        title: 'Como funcionam as comunidades?',
+        message:
+            'As comunidades são normalmente lideradas por instituições sociais locais que tem um acesso direto aos beneficiários finais e que fazem a sua seleção, de forma a chegar mais rapidamente a quem mais precisa primeiro.',
+    },
+    {
+        title: 'Como posso criar uma comunidade?',
+        message:
+            'Para criar uma comunidade, poderá preencher o formulário, no canto superior direito da listagem das comunidades. Deverá adicionar informação adicional sobre a comunidade, os seus beneficiários e definir as regras de acesso aos fundos por parte dos beneficiários (por exemplo, cada beneficiário poderá pedir $1/dia até acumular um máximo de $500. No momento da criação, e após uma validação, um contrato é submetido na blockchain com essas regras.',
+    },
+    {
+        title: 'Como posso apoiar uma comunidade?',
+        message:
+            'Poderá enviar $cUSD diretamente da sua wallet ou exchange, ou, na app Valora, pode comprar $cUSD diretamente com um cartão de crédito/débito e depois enviar.',
+    },
+    {
+        title:
+            'Vivo em condições de vulnerabilidade. Como posso ter acesso a um rendimento básico? ',
+        message:
+            'Contacte uma instituição social local, que conheça e confie, e peça para criar uma comunidade ou entrar em contacto connosco.',
+    },
+    {
+        title: 'É seguro doar na impactMarket?',
+        message:
+            'Sim. O facto de usarmos blockchain significa que ninguém pode corromper ou censurar qualquer transação.',
+    },
+    {
+        title: 'Que moeda é usada? O que é cUSD? ',
+        message:
+            'A moeda usada como base de todas as transações é o dólar americano, representado por cUSD. cUSD é uma criptomoeda, que opera na rede da Celo, e que vale $1. Estas moedas são convertíveis, a qualquer momento, em doláres.',
+    },
+    { title: 'Onde posso transacionar cUSD? ', message: '' },
+    {
+        title: 'Quanto é a taxa por cada transação?',
+        message:
+            'As taxas de transação na rede da Celo são bastante baixas, normalmente a rondar $0.01 por transação.',
+    },
+    {
+        title: 'Como adquirir ou converter cUSD para a minha moeda?',
+        message: '',
+    },
+    {
+        title:
+            'Sou uma empresa, fundação, marca ou celebridade. Como me posso envolver?',
+        message: '',
+    },
+    { title: 'Como contactar a impactMarket?', message: '' },
+];
 
 function FAQScreen() {
     const navigation = useNavigation();
@@ -24,73 +98,19 @@ function FAQScreen() {
         <>
             <Header title="FAQ" navigation={navigation} hasBack />
             <ScrollView style={styles.contentView}>
-                <List.Accordion
-                    title="Where can I claim?"
-                    expanded={expanded.indexOf('where_can_i_claim') !== -1}
-                    onPress={() => handlePress('where_can_i_claim')}
-                >
-                    <View style={styles.descriptionView}>
-                        <Text style={styles.descriptionText}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Etiam finibus neque id urna ultricies, vitae
-                            auctor purus vulputate. Maecenas viverra augue ac ex
-                            iaculis convallis. Praesent nec orci convallis leo
-                            tristique facilisis nec nec mi.{' '}
-                            <Text
-                                style={{ color: iptcColors.softBlue }}
-                                onPress={() =>
-                                    Linking.openURL('https://google.pt')
-                                }
-                            >
-                                Etiam vestibulum risus maximus
-                            </Text>{' '}
-                            ornare pretium. Nunc posuere ipsum ac aliquam
-                            luctus. Aenean scelerisque tellus vel imperdiet
-                            pulvinar. Sed facilisis nisi sit amet augue mattis
-                            imperdiet. Nulla molestie ut arcu at dapibus. Nunc
-                            dictum nisi est, nec vulputate purus placerat
-                            dictum.
-                        </Text>
-                        <Image
-                            style={{ width: 50, height: 50 }}
-                            source={{
-                                uri:
-                                    'https://reactnative.dev/img/tiny_logo.png',
-                            }}
-                        />
-                        <Text style={styles.descriptionText}>
-                            Mauris ac eros in enim scelerisque commodo.
-                            Suspendisse potenti. Vivamus vel dolor enim.
-                            Interdum et malesuada fames ac ante ipsum primis in
-                            faucibus. Curabitur hendrerit vehicula vestibulum.
-                        </Text>
-                    </View>
-                </List.Accordion>
-                <List.Accordion
-                    title="Do I need to buy crypto to join a community?"
-                    expanded={
-                        expanded.indexOf(
-                            'need_to_buy_crypto_to_join_community'
-                        ) !== -1
-                    }
-                    onPress={() =>
-                        handlePress('need_to_buy_crypto_to_join_community')
-                    }
-                >
-                    <View style={styles.descriptionView}>
-                        <Text style={styles.descriptionText}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Etiam finibus neque id urna ultricies, vitae
-                            auctor purus vulputate. Maecenas viverra augue ac ex
-                            iaculis convallis. Praesent nec orci convallis leo
-                            tristique facilisis nec nec mi.
-                        </Text>
-                    </View>
-                </List.Accordion>
-                <List.Accordion title="How can I communicate with my community?" />
-                <List.Accordion title="How do I use the money I claimed?" />
-                <List.Accordion title="Are communities wallets insured?" />
-                <List.Accordion title="How can I leave my community?" />
+                {ptFAQ.map((faq) => (
+                    <List.Accordion
+                        title={faq.title}
+                        expanded={expanded.indexOf(faq.title) !== -1}
+                        onPress={() => handlePress(faq.title)}
+                    >
+                        <View style={styles.descriptionView}>
+                            <Text style={styles.descriptionText}>
+                                {faq.message}
+                            </Text>
+                        </View>
+                    </List.Accordion>
+                ))}
             </ScrollView>
         </>
     );
