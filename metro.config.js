04d0a15c545586d3acf0c8a13d418ce401380b07
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-11 11:15:31 +0100

    update @celo/contractkit

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,16 @@
-const crypto = require.resolve('crypto-browserify')
-const url = require.resolve('url/')
 module.exports = {
 	resolver: {
 		extraNodeModules: {
-			crypto,
-			url,
+			crypto: require.resolve('crypto-browserify'),
+			url: require.resolve('url/'),
 			fs: require.resolve('expo-file-system'),
 			http: require.resolve('stream-http'),
 			https: require.resolve('https-browserify'),
 			os: require.resolve('os-browserify/browser.js'),
 			stream: require.resolve('readable-stream'),
 			vm: require.resolve('vm-browserify'),
+			net: require.resolve('react-native-tcp'),
+			path: require.resolve('path-browserify'),
 		},
 	},
 }
