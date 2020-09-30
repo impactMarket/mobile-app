8f9bb21ffb7c11bbc52264b0f399281019ba92d1
Bernardo Vieira <bernardo5@live.com.pt>
2020-09-30 20:27:02 +0100

    catch not sending txs

--- src/services/celoWallet.ts
+++ src/services/celoWallet.ts
@@ -2,6 +2,7 @@ import { toTxResult } from '@celo/contractkit/lib/utils/tx-result';
 import { requestTxSig, FeeCurrency, waitForSignedTxs } from '@celo/dappkit';
 import * as Linking from 'expo-linking';
 import { ContractKit } from '@celo/contractkit';
+import * as Sentry from 'sentry-expo';
 
 async function celoWalletRequest(
     from: string,
@@ -11,26 +12,31 @@ async function celoWalletRequest(
     kit: ContractKit
 ): Promise<any> {
     const dappName = 'impactmarket';
-    const callback = Linking.makeUrl(`${requestId}/`);
-
-    requestTxSig(
-        kit,
-        [
-            {
-                from,
-                to,
-                tx: txObject,
-                feeCurrency: FeeCurrency.cUSD,
-            },
-        ],
-        { requestId, dappName, callback }
-    );
-
-    const dappkitResponse = await waitForSignedTxs(requestId);
-    const tx = dappkitResponse.rawTxs[0];
-    return toTxResult(
-        kit.web3.eth.sendSignedTransaction(tx)
-    ).waitReceipt();
+    const callback = Linking.makeUrl(requestId);
+    try {
+        await requestTxSig(
+            kit,
+            [
+                {
+                    from,
+                    to,
+                    tx: txObject,
+                    feeCurrency: FeeCurrency.cUSD,
+                },
+            ],
+            { requestId, dappName, callback }
+        );
+        const dappkitResponse = await waitForSignedTxs(requestId);
+        const tx = dappkitResponse.rawTxs[0];
+        return toTxResult(kit.web3.eth.sendSignedTransaction(tx)).waitReceipt();
+    } catch (e) {
+        Sentry.captureMessage(
+            JSON.stringify(txObject._method),
+            Sentry.Severity.Critical
+        );
+        Sentry.captureException(e);
+        throw new Error(e);
+    }
 }
 
 export { celoWalletRequest };
