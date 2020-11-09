4dc2e4d0a0093557ffd1b7b70763dbac51c23c5b
Bernardo Vieira <bernardo5@live.com.pt>
2020-11-09 14:51:08 +0000

    select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,78 @@
+import { iptcColors } from 'helpers/index';
+import React, { Component } from 'react';
+import {
+    GestureResponderEvent,
+    TouchableWithoutFeedback,
+    View,
+} from 'react-native';
+import { Paragraph, Text } from 'react-native-paper';
+import Svg, { Path } from 'react-native-svg';
+
+interface ISelectProps {
+    label: string;
+    value: string;
+    onPress?: (event: GestureResponderEvent) => void;
+}
+export default class Select extends Component<ISelectProps, {}> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        const { label, value, onPress } = this.props;
+        return (
+            <>
+                <Paragraph
+                    style={{
+                        fontSize: 17,
+                        lineHeight: 17,
+                        letterSpacing: 0.245455,
+                        color: iptcColors.textGray,
+                        marginVertical: 8,
+                    }}
+                >
+                    {label}
+                </Paragraph>
+                <TouchableWithoutFeedback onPress={onPress}>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingVertical: 8,
+                            paddingHorizontal: 15,
+                            elevation: 0,
+                            height: 42,
+                            backgroundColor: 'rgba(206,212,218,0.27)',
+                            borderRadius: 6,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: iptcColors.almostBlack,
+                                opacity: 1,
+                                fontSize: 20,
+                                lineHeight: 24,
+                                textAlignVertical: 'center',
+                            }}
+                        >
+                            {value}
+                        </Text>
+                        <Svg
+                            width={14}
+                            height={9}
+                            viewBox="0 0 14 9"
+                            fill="none"
+                            style={{ alignSelf: 'center' }}
+                        >
+                            <Path
+                                opacity={0.833}
+                                d="M6.997 5.812L11.861.717a.69.69 0 011.005 0l.426.447c.134.14.208.327.208.527s-.074.386-.208.526l-5.79 6.066a.69.69 0 01-.504.217.69.69 0 01-.505-.217L.708 2.223A.757.757 0 01.5 1.696c0-.2.074-.386.208-.527l.426-.446a.69.69 0 011.005 0l4.858 5.089z"
+                                fill="#172B4D"
+                            />
+                        </Svg>
+                    </View>
+                </TouchableWithoutFeedback>
+            </>
+        );
+    }
+}
