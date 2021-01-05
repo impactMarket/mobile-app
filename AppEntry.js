41a0a62309bf510b0235e9aedd19ee7615e0ad08
Bernardo Vieira <bernardo5@live.com.pt>
2021-01-05 19:21:45 +0000

    enableScreens from react-native-screens

--- AppEntry.js
+++ AppEntry.js
@@ -4,6 +4,9 @@ import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { activateKeepAwake } from 'expo-keep-awake';
 
 import App from './App';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 if (__DEV__) {
   activateKeepAwake();
