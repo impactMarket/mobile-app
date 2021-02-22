b172062fd2b1931de35e4f062a0b0f4819e85275
Bernardo Vieira <bernardo@impactmarket.com>
2021-02-22 10:59:55 +0000

    CarouselSlide component

---
+++ src/views/stories/CarouselSlide.tsx
@@ -0,0 +1,46 @@
+import CacheImage from 'components/CacheImage';
+import { ICommunityStory } from 'helpers/types/endpoints';
+import React from 'react';
+import { useWindowDimensions, View } from 'react-native';
+
+export default function CarouselSlide({ data }: { data: ICommunityStory }) {
+    const dimensions = useWindowDimensions();
+
+    return (
+        <View
+            style={{
+                height: dimensions.height,
+                width: dimensions.width,
+                flexDirection: 'row',
+            }}
+        >
+            {data.media && (
+                <View style={{ flexDirection: 'row' }}>
+                    <CacheImage
+                        source={{
+                            uri: data.media,
+                        }}
+                        style={{
+                            width: dimensions.width,
+                            height: dimensions.height,
+                            resizeMode: 'cover',
+                            zIndex: 4,
+                            position: 'absolute',
+                        }}
+                        blurRadius={8}
+                    />
+                    <CacheImage
+                        source={{
+                            uri: data.media,
+                        }}
+                        style={{
+                            width: dimensions.width,
+                            resizeMode: 'contain',
+                            zIndex: 5,
+                        }}
+                    />
+                </View>
+            )}
+        </View>
+    );
+}
