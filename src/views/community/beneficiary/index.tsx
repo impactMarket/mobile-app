9bc893509f5110d5031b1c69495dd8c6a039b352
Jonathan Ferreira <jonathanferreira23@gmail.com>
2021-03-05 18:07:40 +0000

    replace env on configs in details view

--- src/views/community/beneficiary/index.tsx
+++ src/views/community/beneficiary/index.tsx
@@ -320,7 +320,10 @@ function BeneficiaryScreen() {
                             </View>
                         </View>
                         <Claim
-                            claimAmount={community.contract.claimAmount}
+                            claimAmount={community.contract.claimAmount.replace(
+                                /,/g,
+                                '.'
+                            )}
                             updateClaimedAmount={updateClaimedAmountAndCache}
                             cooldownTime={cooldownTime}
                             updateCooldownTime={getNewCooldownTime}
