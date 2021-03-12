a614dea476aa9310b2642914f7b9b0a19b311171
Jonathan Ferreira <jonathanferreira23@gmail.com>
2021-03-12 14:42:42 +0000

    refact threeDots to be reusable

--- src/navigator/header/CommunityManager.tsx
+++ src/navigator/header/CommunityManager.tsx
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import { IRootState } from 'helpers/types/state';
 import FAQSvg from 'components/svg/header/FaqSvg';
 import ThreeDotsSvg from 'components/svg/header/ThreeDotsSvg';
-import React from 'react';
-import { View } from 'react-native';
+import Button from 'components/core/Button';
+import { Screens } from 'helpers/constants';
+import i18n from 'assets/i18n';
 
 function CommunityManager() {
+    const navigation = useNavigation();
+    const [openThreeDotsMenu, setOpenThreeDotsMenu] = useState(false);
+
+    const community = useSelector(
+        (state: IRootState) => state.user.community.metadata
+    );
     return (
         <View
             style={{
@@ -13,7 +26,25 @@ function CommunityManager() {
             }}
         >
             <FAQSvg />
-            <ThreeDotsSvg style={{ marginLeft: 8.4, marginRight: 16 }} />
+            <ThreeDotsSvg
+                setOpenThreeDotsMenu={setOpenThreeDotsMenu}
+                openThreeDotsMenu={openThreeDotsMenu}
+                style={{ marginLeft: 8.4, marginRight: 16 }}
+            >
+                <Button
+                    modeType="gray"
+                    bold
+                    style={{ marginVertical: 10, width: '100%' }}
+                    onPress={() => {
+                        setOpenThreeDotsMenu(false);
+                        navigation.navigate(Screens.CommunityDetails, {
+                            communityId: community.publicId,
+                        });
+                    }}
+                >
+                    {i18n.t('viewAsPublic')}
+                </Button>
+            </ThreeDotsSvg>
         </View>
     );
 }
