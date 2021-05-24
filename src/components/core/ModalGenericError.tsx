b2ecbb762a53911cc9605201a708aea258145455
Jonathan Ferreira <jonathanferreira23@gmail.com>
2021-05-24 19:15:50 +0100

    fix modal design

--- src/components/core/ModalGenericError.tsx
+++ src/components/core/ModalGenericError.tsx
@@ -23,80 +23,77 @@ const ModalGenericError: React.FC<ErrorProps> = ({
     return (
         <RNPortal>
             <Modal visible dismissable={false}>
-                <Card style={{ marginHorizontal: 22, borderRadius: 12 }}>
+                <Card
+                    style={{
+                        marginHorizontal: 22,
+                        borderRadius: 12,
+                        paddingHorizontal: 22,
+                        paddingVertical: 16,
+                    }}
+                >
                     <View
                         style={{
-                            paddingVertical: 14,
-                            height: 260,
-                            width: '88%',
+                            flexDirection: 'row',
                             alignItems: 'center',
-                            alignSelf: 'center',
+                            justifyContent: 'space-between',
+                            width: '100%',
+                            marginBottom: 13.5,
                         }}
                     >
-                        <View
+                        <Text
                             style={{
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                                width: '100%',
-                                marginBottom: 13.5,
+                                fontFamily: 'Manrope-Bold',
+                                fontSize: 18,
+                                lineHeight: 20,
+                                textAlign: 'left',
                             }}
                         >
-                            <Text
-                                style={{
-                                    fontFamily: 'Manrope-Bold',
-                                    fontSize: 18,
-                                    lineHeight: 20,
-                                    textAlign: 'left',
-                                }}
-                            >
-                                {title}
-                            </Text>
-                            <CloseStorySvg onPress={closeFn} />
-                        </View>
-                        <View
-                            style={{
-                                paddingVertical: 16,
-                                paddingHorizontal: 22,
-                                borderStyle: 'solid',
-                                borderColor: '#EB5757',
-                                borderWidth: 2,
-                                borderRadius: 8,
-                                width: '100%',
-                                flexDirection: 'row',
-                                marginBottom: 20,
-                            }}
-                        >
-                            <WarningRedTriangle
-                                style={{
-                                    alignSelf: 'flex-start',
-                                    marginRight: 16,
-                                    marginTop: 8,
-                                }}
-                            />
-                            <Text
-                                style={{
-                                    fontFamily: 'Inter-Regular',
-                                    fontSize: 14,
-                                    lineHeight: 24,
-                                    color: ipctColors.almostBlack,
-                                    textAlign: 'justify',
-                                    marginRight: 36,
-                                }}
-                            >
-                                {description}
-                            </Text>
-                        </View>
-
-                        <Button
-                            modeType="gray"
-                            bold
-                            style={{ width: '100%' }}
-                            onPress={btnFn}
-                        >
-                            {btnString}
-                        </Button>
+                            {title}
+                        </Text>
+                        <CloseStorySvg onPress={closeFn} />
                     </View>
+                    <View
+                        style={{
+                            paddingVertical: 16,
+                            paddingHorizontal: 22,
+                            borderStyle: 'solid',
+                            borderColor: '#EB5757',
+                            borderWidth: 2,
+                            borderRadius: 8,
+                            width: '100%',
+                            flexDirection: 'row',
+                            marginBottom: 16,
+                        }}
+                    >
+                        <WarningRedTriangle
+                            style={{
+                                alignSelf: 'flex-start',
+                                marginRight: 16,
+                                marginTop: 8,
+                            }}
+                        />
+                        <Text
+                            style={{
+                                fontFamily: 'Inter-Regular',
+                                fontSize: 14,
+                                lineHeight: 24,
+                                color: ipctColors.almostBlack,
+                                textAlign: 'justify',
+                                marginRight: 36,
+                            }}
+                        >
+                            {description}
+                        </Text>
+                    </View>
+
+                    <Button
+                        modeType="gray"
+                        bold
+                        style={{ width: '100%' }}
+                        onPress={btnFn}
+                    >
+                        {btnString}
+                    </Button>
                 </Card>
             </Modal>
         </RNPortal>
