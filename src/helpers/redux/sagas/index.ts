9ce0f5bd56fb54714c96fe0be3230accbec45a66
Jonathansoufer <jonathanferreira23@gmail.com>
2021-08-12 15:27:01 +0100

    add stories back to rootSagas

--- src/helpers/redux/sagas/index.ts
+++ src/helpers/redux/sagas/index.ts
@@ -2,10 +2,12 @@ import { all, spawn } from 'redux-saga/effects';
 
 import communitiesSaga from './communities';
 import { startWatchingNetworkConnectivity } from './offline';
+import stories from './stories';
 
 export default function* rootSagas() {
     return yield all([
         spawn(startWatchingNetworkConnectivity),
         communitiesSaga,
+        stories,
     ]);
 }
