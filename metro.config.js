f1a27c79177fbb553eaf27c37c5b0e65098789a8
Bernardo Vieira <bernardo@impactmarket.com>
2021-09-22 10:31:58 +0100

    new build (#216)
    * add @expo/metro-config
    
    * new metro config
    
    * new eas config
    
    * add main to app.json
    
    * update circleci config to use eas

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,19 @@
-module.exports = {
-	resolver: {
-		extraNodeModules: {
-			crypto: require.resolve('crypto-browserify'),
-			url: require.resolve('url/'),
-			fs: require.resolve('expo-file-system'),
-			http: require.resolve('stream-http'),
-			https: require.resolve('https-browserify'),
-			os: require.resolve('os-browserify/browser.js'),
-			stream: require.resolve('readable-stream'),
-			vm: require.resolve('vm-browserify'),
-			net: require.resolve('react-native-tcp'),
-			path: require.resolve('path-browserify'),
-		},
-	},
-}
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+    crypto: require.resolve('crypto-browserify'),
+    url: require.resolve('url/'),
+    fs: require.resolve('expo-file-system'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser.js'),
+    stream: require.resolve('readable-stream'),
+    vm: require.resolve('vm-browserify'),
+    net: require.resolve('react-native-tcp'),
+    path: require.resolve('path-browserify'),
+    ...config.resolver?.resolver?.extraNodeModules,
+};
+
+module.exports = config;
