640e997f73b9991bb7103cac7513e63121f924f7
Bernardo Vieira <bernardo@impactmarket.com>
2021-10-06 14:46:01 +0100

    finish extract translations

--- src/assets/i18n/index.ts
+++ src/assets/i18n/index.ts
@@ -1,6 +1,6 @@
+import en from '@impact-market/translations/mobile/en.json';
+import pt from '@impact-market/translations/mobile/pt.json';
 import i18next from 'i18next';
-import en from 'impact-market-translations/mobile/en.json';
-import pt from 'impact-market-translations/mobile/pt.json';
 import { initReactI18next } from 'react-i18next';
 import 'moment/locale/pt';
 
